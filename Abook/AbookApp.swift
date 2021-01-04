255ca90c8f9418f230c10560d05835d191aeb890
gtg7784 <tae.gun7784@gmail.com>
2021-01-04 10:13:57 +0900

    Initial Commit

---
+++ Abook/AbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbookApp.swift
+//  Abook
+//
+//  Created by 고태건 on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct AbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
